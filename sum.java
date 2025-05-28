9c08dc71b8c3864dbabf6b05956134a2344e781a
sarthak <sarthak.shinde2006@gmail.com>
2025-05-28 19:38:49 +0530

    Sum of Two Number

---
+++ sum.java
@@ -0,0 +1,14 @@
+import java.util.*;
+
+public class sum {
+    public static void main(String args[]){
+        Scanner sc=new Scanner(System.in);
+        int n=sc.nextInt();
+        int m=sc.nextInt();
+        int sum;
+        sum=n+m;
+        System.out.println(m+n);
+        sc.close();
+    }
+    
+}
