afe112252b4bf29aea7e4c7b19d6f8910f8dd9e9
sarthak <sarthak.shinde2006@gmail.com>
2025-07-12 16:43:44 +0530

    Important concept can work

--- problem.java
+++ problem.java
@@ -1,4 +1,4 @@
-import java.util.*;
+/*import java.util.*;
 public class problem {
     public static void main(String args[]){
         Scanner sc=new Scanner(System.in);
@@ -18,6 +18,25 @@ public class problem {
         sc.close();
     }
     
-    
-    
+}*/
+
+//WAP which contains the function to conert to string and to convert character to integer.
+import java.util.*;
+public class problem{
+    public static void main(String args[]){
+        Scanner sc=new Scanner(System.in);
+        int n=sc.nextInt();
+        String str=Integer.toString(n);
+        for(int i=0;i<str.length();i++){
+            char ch=str.charAt(i);
+            int digit=ch-'0';  //This is conversion of character to string.
+
+            if(digit%2==0){
+                System.out.print(digit);
+            }else{
+                System.out.print("*");
+            }
+        }
+    }
+
 }
