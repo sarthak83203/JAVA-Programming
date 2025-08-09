900e4818bf4dddca83a787f90a369cc502e3364d
sarthak <sarthak.shinde2006@gmail.com>
2025-08-09 12:31:23 +0530

    Shift question

---
+++ important_shift.java
@@ -0,0 +1,66 @@
+/*Mason is participating in a coding challenge where he must manipulate an integer array. His task is to replace every element in the array with the next greatest element to its right. The last element of the array remains unchanged, as there is no element to its right.
+
+
+
+Your job is to help Mason write a program that performs this transformation and outputs the modified array.
+
+Input format :
+The first line of input contains an integer n representing the number of elements in the array.
+
+The second line of input contains n space-separated integers representing the elements of the array.
+
+Output format :
+The output prints the modified array of n integers, where each element (except the last one) is replaced by the maximum element to its right, and the last element remains unchanged.
+
+
+
+Refer to the sample output for formatting specifications.
+
+Code constraints :
+The given testcases fall under the following constraints:
+
+1 ≤ n ≤ 100
+
+−10⁹ ≤ arr[i] ≤ 10⁹
+
+Sample test cases :
+Input 1 :
+6
+12 3 91 15 12 14
+Output 1 :
+91 91 15 14 14 14
+Input 2 :
+5
+10 3 7 2 6
+Output 2 :
+7 7 6 6 6
+Input 3 :
+3
+1 2 3
+Output 3 :
+3 3 3
+*/
+import java.util.*;
+public class important_shift {
+    public static void main(String args[]){
+        Scanner sc=new Scanner(System.in);
+        int n=sc.nextInt();
+        int arr[]=new int[n];
+        for(int i=0;i<n;i++){
+            arr[i]=sc.nextInt();
+        }
+       for(int i=0;i<n-1;i++){
+           int max=arr[i+1];
+           for(int j=i+1;j<n;j++){
+               if(arr[j]>max){
+                   max=arr[j];
+               }
+           }
+           arr[i]=max;
+       }
+       for(int i=0;i<n;i++){
+           System.out.print(arr[i]+" ");
+       }
+}
+    
+}
